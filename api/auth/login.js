b999eada290153217995f00aad1fced043a7f9a4
GrimBlackManOfficial <85339236+GrimBlackManOfficial@users.noreply.github.com>
2026-05-11 01:14:17 +0200

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,1 +1,4 @@
-const guildId = "ZDE_VLOZ_ID_TVÉHO_SERVERU";
+export default function handler(req, res) {
+    const url = `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.DISCORD_REDIRECT_URI)}&response_type=code&scope=identify%20guilds.members.read`;
+    res.redirect(url);
+}
